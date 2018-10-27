2c05bbfa164e1e680d9613f2ada1dd5cf1a339d9
WesternGreen <westerngreen@126.com>
2018-10-27 12:07:55 +0800

    add main.cpp, now it can analyze some simple files

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "buffer.h"
+#include "automata.h"
+#include "displayer.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char ** argv){
+	vector<string> fileNames;
+	for (int i = 1; i < argc; ++i){
+		fileNames.push_back(argv[i]);
+	}
+	if (argc == 1){
+		string str;
+		cout << "Please input a file's name:";
+		getline(cin, str);
+		fileNames.push_back(str);
+	}
+	for (int i = 0; i < fileNames.size(); ++i){
+		Buffer buf(fileNames[i]);
+		Automata automata(buf);
+		Displayer displayer(automata);
+		displayer.start();
+	}
+	system("pause");
+}
